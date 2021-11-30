637cf1b09432108c0282fe2b40b6394da49b8814
Azehreya Franklin <70592207+Azehreya@users.noreply.github.com>
2021-11-30 12:25:12 -0500

    [7B] v0.2
    added window surface and set mode

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,5 +1,10 @@
-# My First PyGame, Azehreya Franklin, 11/30/21, 12:12PM v0.1 
+# My First PyGame, Azehreya Franklin, 11/30/21, 12:23PM v0.2 
 
 import pygame, sys
 from pygame.locals import *
 
+# Initialize PyGame
+pygame.init()
+
+# Setup the window to draw on.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
