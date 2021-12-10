37d81933b21ba260196b635fb38031cc51a45947
Azehreya Franklin <70592207+Azehreya@users.noreply.github.com>
2021-12-10 13:07:37 -0500

    v0.3 [7B]
    Set up the color values

--- simple_animation.py
+++ simple_animation.py
@@ -1,7 +1,9 @@
-# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 12:59PM, v0.2
+# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 1:06PM, v0.3
 
 import pygame, sys, time
 from pygame.locals import *
+
+from myFirstPygame import GREEN, RED, WHITE
 
 # Setup PyGame
 pygame.init()
@@ -19,3 +21,9 @@ UPLEFT = 'upleft'
 UPRIGHT = 'upright'
 
 MOVESPEED = 4
+
+# Set up color values 
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
