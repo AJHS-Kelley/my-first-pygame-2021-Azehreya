36c7db4b38bf75e16b196906f07e6694439b3428
Azehreya Franklin <70592207+Azehreya@users.noreply.github.com>
2021-12-10 13:00:31 -0500

    v0.2 [7B]
    Established the directions for diagonal movement and movement speed

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 11:56AM, v0.1
+# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 12:59PM, v0.2
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
+# Setup the direction Variables. 
+DOWNLEFT = "downleft"
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
