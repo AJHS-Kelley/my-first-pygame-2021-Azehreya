ebb699c2a0cd17a6588ea2e68400678289fd4aa4
Azehreya Franklin <70592207+Azehreya@users.noreply.github.com>
2021-12-10 11:46:50 -0500

    v0.0 [7B]
    added program information

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,6 +1,7 @@
-# My First PyGame, Azehreya Franklin, 11/30/21, 1:27PM v0.4 
+# My First PyGame, Azehreya Franklin, 12/10/21, 11:34PM v0.5 
 
 import pygame, sys
+from pygame import PixelArray, draw
 from pygame.locals import *
 
 # Initialize PyGame
@@ -9,7 +10,6 @@ pygame.init()
 # Setup the window to draw on.
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('My First PyGame')
-
 
 # Setup color values.
 BLACK = (0, 0, 0)
@@ -23,7 +23,45 @@ TEAL = (0, 50.2, 50.2)
 basicFont = pygame.font.SysFont(None, 48)
 
 # Setup the text
-text = basicFont.render('Hello, world!, True, WHITE, BLUE')
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx 
 textRect.centery = windowSurface.get_rect().centery
+
+# Fill in window with background color.
+windowSurface.fill(TEAL)
+
+# Draw a polygon on the screen.
+pygame.draw.polygon(windowSurface, GREEN, ((146,0), (291,106), (236,277), (56,277), (0,106)))
+
+#Draw Lines on the screen
+pygame.draw.line(windowSurface, RED, (60,60), (120,60), 4)
+pygame.draw.line(windowSurface, WHITE, (75,60), (60,75), 2)
+pygame.draw.line(windowSurface, BLUE, (0,150), (150,0), 1)
+
+# draw a circle.
+pygame.draw.circle(windowSurface, BLACK, (300,50), 20, 0)
+
+# draw a ellipse.
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
+
+# draw the test rectangle.
+pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+
+# Create Pixel Array
+pixArray = pygame.PixelArray(windowSurface)
+pixArray[480][380] = BLUE
+del pixArray
+
+# Draw the text onto the surface
+windowSurface.blit(text, textRect)
+
+# Update Pygame Display
+pygame.display.update()
+
+# Run game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
---
+++ simple_animation.py
@@ -0,0 +1,2 @@
+# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 11:44AM, v0.0
+
