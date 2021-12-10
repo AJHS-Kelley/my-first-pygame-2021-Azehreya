d0fdad2386a526f3f34777aa8c72bd4db4c4d8ab
Azehreya Franklin <70592207+Azehreya@users.noreply.github.com>
2021-12-10 11:57:38 -0500

    v0.1 [7B]
    imported modules for PyGame , system and line
    initialized the PyGame Module

--- simple_animation.py
+++ simple_animation.py
@@ -1,2 +1,13 @@
-# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 11:44AM, v0.0
+# Simple Animation with PyGame, Azehreya Franklin, 12/10/21, 11:56AM, v0.1
 
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+
+# Setup the window 
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
